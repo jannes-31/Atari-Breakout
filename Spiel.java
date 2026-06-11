689d34bc10747231bbcc1ec03e21de531d7bd62e
jannes-31 <jannesgregorhoppe@gmail.com>
2026-06-11 13:08:24 +0200

    Update Spiel.java

--- Spiel.java
+++ Spiel.java
@@ -9,10 +9,6 @@ class Spiel
     bloecke[] reihe1;
     bloecke[] reihe2;
     bloecke[] reihe3;
-    Text punkteAnzeige;
-    Text ergebnisText;
-    Text hinweisText;
-    Text countdownText;
     bloecke[][] bloeckeArray;
     Text punkteAnzeige,ergebnis,hinweis,countdown;
     
@@ -26,33 +22,29 @@ class Spiel
         schlaeger = new Rectangle(250, 365, 100, 12, Color.cyan);
         ball = new Circle(292, 192, 8, Color.white);
 
-        int spalten = 10;
-        double bBreite = 54;
-        double bHoehe = 20;
-        double abstandX = 5;
-        double startX = (600 - (spalten * bBreite + (spalten - 1) * abstandX)) / 2.0;
 
-        reihe1 = new bloecke[spalten];
-        reihe2 = new bloecke[spalten];
-        reihe3 = new bloecke[spalten];
-        for (int i = 0; i < spalten; i++)
+
+        reihe1 = new bloecke[10];
+        reihe2 = new bloecke[10];
+        reihe3 = new bloecke[10];
+        
+        for (int i = 0; i < 10; i++)
         {
-            double x = startX + i * (bBreite + abstandX);
-            reihe1[i] = new bloecke(x, 45, bBreite, bHoehe, Color.red,    3);
-            reihe2[i] = new bloecke(x, 71, bBreite, bHoehe, Color.orange, 2);
-            reihe3[i] = new bloecke(x, 97, bBreite, bHoehe, Color.yellow, 1);
+            reihe1[i] = new bloecke(2 + i*60, 45, 54, 20, Color.red,3);
+            reihe2[i] = new bloecke(2 + i*60, 71, 54, 20, Color.orange,2);
+            reihe3[i] = new bloecke(2 + i*60, 97, 54, 20, Color.yellow,1);
         }
 
         punkte = 0;
         punkteAnzeige = new Text(10, 20, "Punkte: 0", Color.white);
-        punkteAnzeige.setFontSansSerif(true, 18);
+        punkteAnzeige.setFontMonospaced(true, 18);
 
         ergebnis = new Text(60, 185, "", Color.red);
         ergebnis.setFontMonospaced(true, 26);
         ergebnis.setHidden(true);
 
         hinweis = new Text(175, 230, "Enter = Neustart", Color.white);
-        hinweis.setFontSansSerif(false, 20);
+        hinweis.setFontMonospaced(false, 20);
         hinweis.setHidden(true);
 
         countdown = new Text(262, 140, "3", Color.white);
@@ -83,7 +75,7 @@ class Spiel
             {
                 if (fenster.keyLeftPressed() && schlaeger.getShapeX() > 0)
                     schlaeger.move(-7);
-                if (fenster.keyRightPressed() && schlaeger.getShapeX() + 100 < 600)
+                if (fenster.keyRightPressed() && schlaeger.getShapeX() < 500)
                     schlaeger.move(7);
 
                 ball.move(dx, dy);
@@ -91,9 +83,18 @@ class Spiel
                 double bx = ball.getShapeX();
                 double by = ball.getShapeY();
 
-                if (bx <= 0 && dx < 0)        dx = -dx;
-                if (bx + 16 >= 600 && dx > 0) dx = -dx;
-                if (by <= 0 && dy < 0)         dy = -dy;
+                if (bx <= 0)
+                {
+                   dx = -dx; 
+                }
+                if (bx + 16 >= 600)
+                {
+                    dx = -dx;
+                }
+                if (by <= 0)
+                {
+                    dy = -dy;
+                }
 
                 if (dy > 0 && ball.intersects(schlaeger))
                 {
@@ -103,7 +104,7 @@ class Spiel
                 }
 
                 boolean blockGetroffen = false;
-                for (int i = 0; i < spalten && !blockGetroffen; i++)
+                for (int i = 0; i < 10 && !blockGetroffen; i++)
                 {
                     if (reihe1[i].trifft(ball)) { punkte += reihe1[i].gibWert(); reihe1[i].treffer(); dy = -dy; blockGetroffen = true; }
                     else if (reihe2[i].trifft(ball)) { punkte += reihe2[i].gibWert(); reihe2[i].treffer(); dy = -dy; blockGetroffen = true; }
@@ -123,7 +124,7 @@ class Spiel
                 }
 
                 boolean alleWeg = true;
-                for (int i = 0; i < spalten; i++)
+                for (int i = 0; i < 10; i++)
                 {
                     if (reihe1[i].istAktiv() || reihe2[i].istAktiv() || reihe3[i].istAktiv())
                         alleWeg = false;
@@ -152,7 +153,7 @@ class Spiel
             schlaeger.moveTo(250, 365);
             punkte = 0;
             punkteAnzeige.setText("Punkte: 0");
-            for (int i = 0; i < spalten; i++)
+            for (int i = 0; i < 10; i++)
             {
                 reihe1[i].zuruecksetzen();
                 reihe2[i].zuruecksetzen();
